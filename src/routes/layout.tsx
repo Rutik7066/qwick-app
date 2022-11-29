0c02f25fb42a70ff3bc2e274da06ced1aa6c4684
rutikthakre <rutikthakre@gmail.com>
2022-11-30 01:59:35 +0530

    useMount

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,12 +4,7 @@ import { component$, Slot } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-      <head>
-        <meta
-          http-equiv="Content-Security-Policy"
-          content="upgrade-insecure-requests"
-        />
-      </head>
+      
       <main>
         <section>
           <Slot />
