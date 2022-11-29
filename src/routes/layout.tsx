2682c66bf02e7db95178ebc626784a77bc106aca
rutikthakre <rutikthakre@gmail.com>
2022-11-30 01:56:40 +0530

    meta

--- src/routes/layout.tsx
+++ src/routes/layout.tsx
@@ -4,16 +4,18 @@ import { component$, Slot } from '@builder.io/qwik';
 export default component$(() => {
   return (
     <>
-      
+      <head>
+        <meta
+          http-equiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </head>
       <main>
-       
         <section>
           <Slot />
         </section>
       </main>
-      <footer>
-      
-      </footer>
+      <footer></footer>
     </>
   );
 });
