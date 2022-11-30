55093fdc14f5106e93e9efe1c517be272ce7474d
rutikthakre <rutikthakre@gmail.com>
2022-11-30 17:15:09 +0530

    test

--- src/routes/photogallery/index.tsx
+++ src/routes/photogallery/index.tsx
@@ -24,6 +24,7 @@ export default component$(() => {
       uid +
       "&aws_id=" +
       folderid;
+    
     console.log(url);
     const res = await fetch(url);
     store.data = await res.json();
