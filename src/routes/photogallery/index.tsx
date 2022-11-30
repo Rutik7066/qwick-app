57cc1fe6adcf798e2743e11ca56e43670e7f4e50
rutikthakre <rutikthakre@gmail.com>
2022-11-30 20:57:49 +0530

    useClientEffect$

--- src/routes/photogallery/index.tsx
+++ src/routes/photogallery/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useServerMount$, useStore } from "@builder.io/qwik";
+import { component$, useClientEffect$, useServerMount$, useStore } from "@builder.io/qwik";
 import { useLocation, useNavigate } from "@builder.io/qwik-city";
 
 export default component$(() => {
@@ -21,7 +21,7 @@ export default component$(() => {
   );
   const nav = useNavigate();
 
-  useServerMount$(async () => {
+  useClientEffect$(async () => {
     console.log("Client : ", loc.query.uid, loc.query.folder);
     console.log("Server : ", store.uid, store.folderid);
     const url =
